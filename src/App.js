f2244c3ac234ffa124647fdf992ce280887f57cb
hossam2671 <108755474+hossam2671@users.noreply.github.com>
2023-07-12 18:53:31 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import UserProfile from './Pages/UserProfile';
+
+function App() {
+  return (
+    <UserProfile />
+  );
+}
+
+export default App;
