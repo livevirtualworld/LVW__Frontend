9803990d2f385593a5a3f48df9b7ae9b0bbb1966
Fxkhalil0 <fxkhalil0@gmail.com>
2024-02-19 14:55:45 +0200

    ..

--- src/Pages/ContactUs/Form.js
+++ src/Pages/ContactUs/Form.js
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './Form.module.css'
+import axios from 'axios'
 
 
 
 
 
 function Form() {
-    return ( 
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = { fullName, email, subject, message };
+        try {
+            const response = await axios.post('http://localhost:5000/user/contactus', formData);
+            console.log(response.data);
+            setFullName('');
+            setEmail('');
+            setSubject('');
+            setMessage('');
+        } catch (error) {
+            console.error('Error:', error.response.data.message);
+            // Handle error, show error message, etc.
+        }
+    };
+    return (
         <>
-        <div className={style['container']}>
-        <h1>Ask a question</h1>
-        <p>Fill out our form and well get back to you in 24 hours.</p>
-        <form>
-            <input type="text" placeholder="Full Name"/>
-            <input type="email" placeholder="Email"/>
-            <input type="text" placeholder="Subject"/>
-            <textarea placeholder="Message"></textarea>
-            <div className={style['button']}>
-                <button>Send Message</button>
+            <div className={style['container']}>
+                <h1>Ask a question</h1>
+                <p>Fill out our form and well get back to you in 24 hours.</p>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Full Name" onChange={(e) => setFullName(e.target.value)} />
+                    <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="text" placeholder="Subject" onChange={(e) => setSubject(e.target.value)} />
+                    <textarea placeholder="Message" onChange={(e) => setMessage(e.target.value)}></textarea>
+                    <div className={style['button']}>
+                        <button>Send Message</button>
+                    </div>
+                </form>
             </div>
-        </form>
-        </div>
         </>
-     );
+    );
 }
 
 export default Form;
