b92660ba8659b1a4d3aca040f95bcc627932d3db
Fxkhalil0 <fxkhalil0@gmail.com>
2024-04-03 16:43:17 +0200

    ..

--- src/Pages/WhoWeAre/WhoWeAre.js
+++ src/Pages/WhoWeAre/WhoWeAre.js
@@ -5,44 +5,44 @@ import style from './WhoWeAre.module.css'
 import Footer from "../Footer/Footer";
 import OurTeam from "./OurTeam";
 
-const teamDescriptions = [
-    {
-        name: 'Sanad Hamdan (Chief Executive Officer)',
-        description: "Founder and CEO of LVW, is a seasoned professional with a robust background in adventure tourism, classic tourism, team-building, and program management. From his beginnings as a Canyon Guide to orchestrating international events and leading operations in diverse roles, Sanad's expertise uniquely enriches LVW. His commitment to seamless experiences, coupled with a deep understanding of operational efficiency, positions LVW for significant growth under his visionary leadership."
-    },
-    {
-        name: 'Enzo La Rosa (Chief Finance Officer)',
-        description: "With 15 years of professional experience, a seasoned financial expert and business coach, brings his financial acumen and strategic planning skills to the team as the company's CFO. His experience in finance ensures that LVW operates with a sound financial foundation and is well-positioned for future growth. Brings over a decade of diverse expertise, ranging from programming to strategic consulting. With a meticulous focus on operational efficiency, cost analysis, and a flair for storytelling, Enzo enriches LVW's team with a profound understanding of business practices and a commitment to fostering growth."
-    },
-    {
-        name: 'Arianna Colombani (Director of Education & Accessibility) ',
-        description: "Founder and CEO of LVW, is a seasoned professional with a robust background in adventure tourism, classic tourism, team-building, and program management. From his beginnings as a Canyon Guide to orchestrating international events and leading operations in diverse roles, Sanad's expertise uniquely enriches LVW. His commitment to seamless experiences, coupled with a deep understanding of operational efficiency, positions LVW for significant growth under his visionary leadership."
-    },
-    {
-        name: 'Rana Khalil (Chief Marketing Officer)',
-        description: "As the Chief Marketing Officer at LVW, Rana brings a dynamic blend of strategic marketing expertise and a passion for driving brand success. She is a seasoned marketing professional with over 20 years of multifaceted expertise. Joining LVW is a personal pledge for her to contribute her passion, skills, and decades of experience to propel the brand forward. She is thrilled to be part of this journey, creating lasting connections and driving sustainable growth that will define LVW's future in the market."
-    },
-    {
-        name: 'Mohammad Ali (Chief Operation Officer)',
-        description: "**No Content Available**"
-    },
-    {
-        name: 'Anisa Bali (Coordinator)',
-        description: "**No Content Available**"
-    },
-    {
-        name: 'Fatma Khalil (Chief Technology Officer)',
-        description: "**No Content Available**"
-    },
-    {
-        name: 'Karim Ashraf (Graphic Designer & Ui Ux)',
-        description: "**No Content Available**"
-    },
-    {
-        name: 'Murad Al-Saidi (Graphic Designer)',
-        description: "**No Content Available**"
-    },
-];
+// const teamDescriptions = [
+//     {
+//         name: 'Sanad Hamdan (Chief Executive Officer)',
+//         description: "Founder and CEO of LVW, is a seasoned professional with a robust background in adventure tourism, classic tourism, team-building, and program management. From his beginnings as a Canyon Guide to orchestrating international events and leading operations in diverse roles, Sanad's expertise uniquely enriches LVW. His commitment to seamless experiences, coupled with a deep understanding of operational efficiency, positions LVW for significant growth under his visionary leadership."
+//     },
+//     {
+//         name: 'Enzo La Rosa (Chief Finance Officer)',
+//         description: "With 15 years of professional experience, a seasoned financial expert and business coach, brings his financial acumen and strategic planning skills to the team as the company's CFO. His experience in finance ensures that LVW operates with a sound financial foundation and is well-positioned for future growth. Brings over a decade of diverse expertise, ranging from programming to strategic consulting. With a meticulous focus on operational efficiency, cost analysis, and a flair for storytelling, Enzo enriches LVW's team with a profound understanding of business practices and a commitment to fostering growth."
+//     },
+//     {
+//         name: 'Arianna Colombani (Director of Education & Accessibility) ',
+//         description: "Founder and CEO of LVW, is a seasoned professional with a robust background in adventure tourism, classic tourism, team-building, and program management. From his beginnings as a Canyon Guide to orchestrating international events and leading operations in diverse roles, Sanad's expertise uniquely enriches LVW. His commitment to seamless experiences, coupled with a deep understanding of operational efficiency, positions LVW for significant growth under his visionary leadership."
+//     },
+//     {
+//         name: 'Rana Khalil (Chief Marketing Officer)',
+//         description: "As the Chief Marketing Officer at LVW, Rana brings a dynamic blend of strategic marketing expertise and a passion for driving brand success. She is a seasoned marketing professional with over 20 years of multifaceted expertise. Joining LVW is a personal pledge for her to contribute her passion, skills, and decades of experience to propel the brand forward. She is thrilled to be part of this journey, creating lasting connections and driving sustainable growth that will define LVW's future in the market."
+//     },
+//     {
+//         name: 'Mohammad Ali (Chief Operation Officer)',
+//         description: "**No Content Available**"
+//     },
+//     {
+//         name: 'Anisa Bali (Coordinator)',
+//         description: "**No Content Available**"
+//     },
+//     {
+//         name: 'Fatma Khalil (Chief Technology Officer)',
+//         description: "**No Content Available**"
+//     },
+//     {
+//         name: 'Karim Ashraf (Graphic Designer & Ui Ux)',
+//         description: "**No Content Available**"
+//     },
+//     {
+//         name: 'Murad Al-Saidi (Graphic Designer)',
+//         description: "**No Content Available**"
+//     },
+// ];
 
 function WhoWeAre() {
     return (
@@ -66,12 +66,12 @@ function WhoWeAre() {
                 <div className={style['body__content']}>
                     <h2>OUR Team:</h2>
                     <OurTeam /> 
-                    {teamDescriptions.map((member, index) => (
+                    {/* {teamDescriptions.map((member, index) => (
                         <div className={style['team__content']} key={index}>
                             <h2>{index + 1}- {member.name}</h2>
                             <p>{member.description}</p>
                         </div>
-                    ))}
+                    ))} */}
                 </div>
                 <div className={style['last__div']}>
                     <h2>Start Your Tour</h2>
